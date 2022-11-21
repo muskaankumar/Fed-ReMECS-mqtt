2ba866c9a7775e6b90a5bac78440256619458802
muskaankumar <99201337+muskaankumar@users.noreply.github.com>
2022-11-21 20:05:05 +0530

    Update fl-run-mc.sh

--- fl-run-mc.sh
+++ fl-run-mc.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 #Ask the user for number of clients
-read -p 'Enter number of Local clients (1 to 32):' nC 
+read -p 'Enter number of Local clients (1 to 32):' nC
 
 
 for ((c=1; c<=nC; c++))
 do
-	konsole --noclose -e python pub-mc.py $c &
+        konsole --noclose -e python pub-mc.py $c &
 done
 
-konsole --noclose -e python sub-mc.py
+konsole --noclose -e python sub-mc.py $nC
+                                        
